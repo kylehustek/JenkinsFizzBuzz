81663195844d8f68f912264cc10314ec672f4880
kylehustek <kylehustek@gmail.com>
2021-10-03 18:48:23 -0400

    Added fizzbuzz test cases

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,19 +1,19 @@
 '''A Simple Python FizzBuzz Script'''
 
 def fizz_buzz(i):
-    '''Prints fizzbuzz of i'''
+    '''Returns fizzbuzz of i'''
     fizzbuzz = ""
     if i % 3 == 0:
         fizzbuzz = fizzbuzz + "Fizz"
     if i % 5 == 0:
         fizzbuzz = fizzbuzz + "Buzz"
-    print(fizzbuzz or i)
+    return(fizzbuzz or i)
 
 def main():
     '''Runs fizz_buzz FIZZ_CAP times'''
     fizz_cap = 15
     for i in range(1,fizz_cap+1):
-        fizz_buzz(i)
+        print(fizz_buzz(i))
 
 if __name__ == "__main__":
     main()
